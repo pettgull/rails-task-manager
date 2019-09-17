98c24b06c64e0f8fcf30172aec147f01c6217587
petter gulliksen <petter-gulliksen@hotmail.com>
2019-09-17 14:43:06 +0200

    trying to get this thing to work, probably a mistake with naming somewhere

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,22 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+
+  end
+
 end
+
+
+
